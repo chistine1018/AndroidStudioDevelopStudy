1063657fb0f25e9c427598149ebf0318d80319cd
哥安 <chistine101814@gmail.com>
2021-05-02 18:32:55 +0800

    [MaterialDesign]pratice MaterialDesign

---
+++ app/src/main/java/e/linyanan/studyapplication/MaterialDesignActivity.java
@@ -0,0 +1,14 @@
+package e.linyanan.studyapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MaterialDesignActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_material_design);
+    }
+}
