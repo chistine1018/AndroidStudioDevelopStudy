e3491b0c964e906d08f02082ba188faec2287cd7
哥安 <chistine101814@gmail.com>
2021-05-14 18:44:45 +0800

    [Feature] Add Snackbar in FABCoordinatorLayoutActivity

--- app/src/main/java/e/linyanan/studyapplication/FABCoordinatorLayoutActivity.java
+++ app/src/main/java/e/linyanan/studyapplication/FABCoordinatorLayoutActivity.java
@@ -2,11 +2,14 @@ package e.linyanan.studyapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class FABCoordinatorLayoutActivity extends AppCompatActivity {
     private TextView mTvMsg;
@@ -28,7 +31,13 @@ public class FABCoordinatorLayoutActivity extends AppCompatActivity {
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            mTvMsg.setText("你按了，FAB");
+            Snackbar.make(view, "你按了按鈕", Snackbar.LENGTH_INDEFINITE).setAction("可以選的字串", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(FABCoordinatorLayoutActivity.this, "彥安好帥", Toast.LENGTH_SHORT).show();
+                }
+            }).setActionTextColor(Color.rgb(0, 255, 0)).show();
+//            mTvMsg.setText("你按了，FAB");
         }
     };
 }
